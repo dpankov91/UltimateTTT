ab5f5dd0ad8fc94b069779420d9f8b5d56ca3757
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-02-25 12:08:20 +0100

    Move.java
    Created Class Move which implemented Interface Move

---
+++ UlitmateTTT/src/ulitmatettt/bll/move/Move.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ulitmatettt.bll.move;
+
+/**
+ *
+ * @author chris
+ */
+public class Move implements IMove {
+    
+    private int x;
+    private int y;
+    
+    public Move(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+    
+    
+    
+}
