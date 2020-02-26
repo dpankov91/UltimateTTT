745f5b8e2e4e4c2e3e8d91b6a792c21375af1a9b
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-02-26 12:18:28 +0100

    Update Field
    implemented methods for class field

--- UlitmateTTT/src/ulitmatettt/bll/field/Field.java
+++ UlitmateTTT/src/ulitmatettt/bll/field/Field.java
@@ -5,6 +5,7 @@
  */
 package ulitmatettt.bll.field;
 
+import java.util.ArrayList;
 import java.util.List;
 import ulitmatettt.bll.move.IMove;
 
@@ -17,22 +18,39 @@ public class Field implements IField
 
     @Override
     public void clearBoard() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         clearBoard();
+         clearMacroboard();
     }
 
     @Override
     public List<IMove> getAvailableMoves() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<IMove> availableMoves = new ArrayList();
+        for(int x = 0; x < macroboard.length; x++) {
+            for(int y = 0; y < macroboard.length; y++) {
+                if(macroboard[x][y].equals(AVAILABLE_FIELD)) {
+                    availableMoves.addAll(getAvailableMovesFromMicroboard(x, y));
+                }
+                    
+            }
+        }
+        return availableMoves;
     }
 
     @Override
     public String getPlayerId(int column, int row) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         return board[column][row];
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for(int x = 0; x < board.length; x++) {
+            for(int y = 0; y < board.length; y++) {
+                if(!board[x][y].equals(EMPTY_FIELD)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
