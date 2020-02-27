256a37005a2216d223d06962d280c516f7df548b
dpankov91 <dpankov91@gmail.com>
2020-02-27 09:39:16 +0100

    changes

--- UlitmateTTT/src/ulitmatettt/bll/field/Field.java
+++ UlitmateTTT/src/ulitmatettt/bll/field/Field.java
@@ -14,44 +14,34 @@ import ulitmatettt.bll.move.Move;
  *
  * @author dpank
  */
-public class Field implements IField
-{
+public class Field implements IField {
 
     private String[][] board;
     private String[][] macroboard;
 
     public Field() {
         board = new String[9][9];
-        macroboard = new String [3][3];
+        macroboard = new String[3][3];
         clearBoard();
     }
 
-     
-    
     @Override
     public void clearBoard() {
-         clearMicroboard();
-         clearMacroboard();
+        clearMicroboard();
+        clearMacroboard();
     }
-    
-    
-     private void clearMicroboard()
-    {
-        for(int x = 0; x < board.length; x++)
-        {
-            for(int y = 0; y < board[x].length; y++)
-            {
+
+    private void clearMicroboard() {
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board[x].length; y++) {
                 board[x][y] = EMPTY_FIELD;
             }
         }
     }
-    
-    private void clearMacroboard()
-    {
-        for(int x = 0; x < macroboard.length; x++)
-        {
-            for(int y = 0; y < macroboard[x].length; y++)
-            {
+
+    private void clearMacroboard() {
+        for (int x = 0; x < macroboard.length; x++) {
+            for (int y = 0; y < macroboard[x].length; y++) {
                 macroboard[x][y] = AVAILABLE_FIELD;
             }
         }
@@ -60,28 +50,23 @@ public class Field implements IField
     @Override
     public List<IMove> getAvailableMoves() {
         List<IMove> availableMoves = new ArrayList();
-        for(int x = 0; x < macroboard.length; x++) {
-            for(int y = 0; y < macroboard.length; y++) {
-                if(macroboard[x][y].equals(AVAILABLE_FIELD)) {
+        for (int x = 0; x < macroboard.length; x++) {
+            for (int y = 0; y < macroboard.length; y++) {
+                if (macroboard[x][y].equals(AVAILABLE_FIELD)) {
                     availableMoves.addAll(getAvailableMovesFromMicroboard(x, y));
                 }
-                    
             }
         }
         return availableMoves;
     }
-    
-    private List<IMove> getAvailableMovesFromMicroboard(int microboardXPosition, int microboardYPosition)
-    {
+
+    private List<IMove> getAvailableMovesFromMicroboard(int microboardXPosition, int microboardYPosition) {
         List<IMove> availableMoves = new ArrayList();
-        int startingXPosition = microboardXPosition*3;
-        int startingYPosition = microboardYPosition*3;
-        for(int x = startingXPosition; x < startingXPosition+3; x++)
-        {
-            for(int y = startingYPosition; y < startingYPosition+3; y++)
-            {
-                if(board[x][y].equals(EMPTY_FIELD))
-                {
+        int startingXPosition = microboardXPosition * 3;
+        int startingYPosition = microboardYPosition * 3;
+        for (int x = startingXPosition; x < startingXPosition + 3; x++) {
+            for (int y = startingYPosition; y < startingYPosition + 3; y++) {
+                if (board[x][y].equals(EMPTY_FIELD)) {
                     availableMoves.add(new Move(x, y));
                 }
             }
@@ -91,14 +76,14 @@ public class Field implements IField
 
     @Override
     public String getPlayerId(int column, int row) {
-         return board[column][row];
+        return board[column][row];
     }
 
     @Override
     public boolean isEmpty() {
-        for(int x = 0; x < board.length; x++) {
-            for(int y = 0; y < board.length; y++) {
-                if(!board[x][y].equals(EMPTY_FIELD)) {
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board.length; y++) {
+                if (!board[x][y].equals(EMPTY_FIELD)) {
                     return false;
                 }
             }
@@ -108,51 +93,41 @@ public class Field implements IField
 
     @Override
     public boolean isFull() {
-       for(int x = 0; x < macroboard.length; x++)
-        {
-            for(int y = 0; y < macroboard[x].length; y++)
-            {
-                if(macroboard[x][y].equals(EMPTY_FIELD) || macroboard[x][y].equals(AVAILABLE_FIELD))
-                {
+        for (int x = 0; x < macroboard.length; x++) {
+            for (int y = 0; y < macroboard[x].length; y++) {
+                if (macroboard[x][y].equals(EMPTY_FIELD) || macroboard[x][y].equals(AVAILABLE_FIELD)) {
                     return false;
                 }
             }
         }
         return true;
-       }
-
+    }
 
     @Override
     public Boolean isInActiveMicroboard(int x, int y) {
-          int macroboardXPosition = x/3;
-        int macroboardYPosition = y/3;
+        int macroboardXPosition = x / 3;
+        int macroboardYPosition = y / 3;
         return macroboard[macroboardXPosition][macroboardYPosition].equals(AVAILABLE_FIELD);
     }
- 
+
     @Override
     public String[][] getBoard() {
-         return board;
+        return board;
     }
 
     @Override
     public String[][] getMacroboard() {
         return macroboard;
-        }
-    
-
-    @Override
-    public void setBoard(String[][] board) 
-    {
-       this.board = board;
     }
 
     @Override
-    public void setMacroboard(String[][] macroboard) 
-    {
+    public void setBoard(String[][] board) {
+        this.board = board;
+    }
+
+    @Override
+    public void setMacroboard(String[][] macroboard) {
         this.macroboard = macroboard;
     }
-    
-      
-           
-    
+
 }
