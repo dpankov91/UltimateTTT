dd75091f8b65f613906420b7e5783fb9d89e605d
dpankov91 <dpankov91@gmail.com>
2020-03-01 11:51:13 +0100

    update in GameManager

--- UlitmateTTT/src/ulitmatettt/bll/game/GameManager.java
+++ UlitmateTTT/src/ulitmatettt/bll/game/GameManager.java
@@ -92,6 +92,8 @@ public class GameManager {
 
         //Update currentPlayer
         currentPlayer = (currentPlayer + 1) % 2;
+        currentState.setMoveNumber(currentState.getMoveNumber()+1);
+        
 
         return true;
     }
